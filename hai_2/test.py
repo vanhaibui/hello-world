2643b6d212ff02c3a603c9668cf3f93d44845da6
vanhaibui <vhbui@umich.edu>
2021-08-26 16:11:19 -0400

    test

---
+++ hai_2/test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 26 16:09:41 2021
+
+@author: HAI
+"""
+
--- hai_sub/test_2_VHB.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 26 15:24:58 2021
-
-@author: HAI
-"""
-
-print("hai dep trai :)")
