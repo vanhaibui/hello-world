fb689e9fc389a58ea3b900beee46fc9538e1501f
VAN HAI BUI <89324710+vanhaibui@users.noreply.github.com>
2021-09-17 14:48:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 import numpy as np
-a=1
-b=2
-c= a+b 
-d=a*b
-print(c)
-print(d)
-print(c*d)
 
-# should be k0 --> da doi sang k0 by HAI
-k0 = np.array([1,2,3,4,5,6])
-print(k0)
-# test result looks great!
+# lam lai nao
+a = np.array([[1,2,3], [4,5,6], [7,8,9]])
+print(a)
+print("hai dai ca")
