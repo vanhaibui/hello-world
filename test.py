62a31bce3de87d7d410d8020d862efc61b1595ae
vanhaibui <vhbui@umich.edu>
2021-09-17 14:46:14 -0400

    hai

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ print(c)
 print(d)
 print(c*d)
 
-# should be k0
-k = np.array([1,2,3,4,5,6])
-print(k)
+# should be k0 --> da doi sang k0 by HAI
+k0 = np.array([1,2,3,4,5,6])
+print(k0)
 # test result looks great!
